8533856232cb2ba9939cdf2211ff980f8efbbabf
Christopher DiStasio <cadistasio@gmail.com>
2020-09-28 21:33:39 -0500

    Adds cart to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { TravelComponent } from './travel/travel.component';
+import { CartComponent } from './cart/cart.component';
+
 
 const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'home', component: HomeComponent},
     {path: 'login', component: LoginComponent},
     {path: 'travel/:roverName', component: TravelComponent},
+    {path: 'cart', component: CartComponent},
 ];
 
 @NgModule({
