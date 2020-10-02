bda8056ecffec12ff67daa7656e2aa675250832a
Christopher DiStasio <cadistasio@gmail.com>
2020-10-02 12:52:21 -0500

    Stubs storefront component

--- src/app/storefront/storefront.component.ts
+++ src/app/storefront/storefront.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { StorefrontService } from './storefront.service';
+import { Storefront } from './storefront'
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-storefront',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StorefrontComponent implements OnInit {
 
-  constructor() { }
+  marsProducts: Storefront[];
 
-  ngOnInit(): void {
+  constructor(private storefrontService : StorefrontService) { }
+
+  ngOnInit() {
+
+    //To Do:
+    // this.http.get<storeFront[]>(this.url).pipe(map(data => data['results']));
+
+    this.storefrontService.getAllMarsProducts().subscribe(data => this.marsProducts = data);
+    console.log(this.marsProducts);
+    for (let storefront of this.marsProducts){
+      this.storefrontService.getMarsProductImage(storefront).subscribe(data => storefront = data);
+    }
   }
 
 }
